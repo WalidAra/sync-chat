c865a27a96accb278f98e6255744c6ed7c654e56
WalidAra <arawalid90@gmail.com>
2024-07-05 12:55:08 +0100

    backend added jwt helper

---
+++ backend/helpers/jwt.helper.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET_KEY = process.env.JWT_SECRET;
+
+class JwtHelper {
+  static generateToken(payload, recall) {
+    return jwt.sign(payload, SECRET_KEY, { expiresIn: recall ? "1h" : "15s" });
+  }
+
+  static verifyToken(token) {
+    try {
+      return jwt.verify(token, SECRET_KEY);
+    } catch (error) {
+      return { error: "Token is invalid or expired" };
+    }
+  }
+}
+
+module.exports = JwtHelper;

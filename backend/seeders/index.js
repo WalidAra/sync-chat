39307086e63e65ed73532536e13a0242e5a6f411
WalidAra <arawalid90@gmail.com>
2024-07-07 17:48:48 +0100

    chore(client): applied the seeder of provider and set up prisama client
    you can use prisma service by importing it from config instead of re-init prisma client everytime

---
+++ backend/seeders/index.js
@@ -0,0 +1,18 @@
+const prisma = require("../config/prisma");
+
+const ProviderSeeder = async () => {
+  try {
+    console.log("====================================");
+
+    await prisma.provider.createMany({
+      data: [{ name: "Google" }, { name: "Facebook" }, { name: "Direct" }],
+    });
+
+    console.log("done !!!");
+    console.log("====================================");
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+ProviderSeeder();

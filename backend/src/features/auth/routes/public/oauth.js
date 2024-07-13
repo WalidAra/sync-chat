e1edc0082faf6509238acae3051245b52627a372
Rihab <rihab.arioui@gmail.com>
2024-07-13 16:43:40 +0100

    feat: Implement OAuth with GitHub

--- backend/src/features/auth/routes/public/oauth.js
+++ backend/src/features/auth/routes/public/oauth.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const passport = require("passport");
 
+const prisma = require("../../../../../config/prisma");
+const axios = require("axios");
+const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } = process.env;
+
 router.get(
   "/google",
   passport.authenticate("google", { scope: ["profile", "email"] })
@@ -19,5 +23,87 @@ router.get(
     }
   }
 );
+
+router.get("/github", (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}`
+  );
+});
+
+router.get("/github/callback", async (req, res) => {
+  const code = req.query.code;
+  if (!code) {
+    return res.status(400).send("Code is missing");
+  }
+
+  try {
+    const response = await axios.post(
+      `https://github.com/login/oauth/access_token`,
+      {
+        client_id: GITHUB_CLIENT_ID,
+        client_secret: GITHUB_CLIENT_SECRET,
+        code,
+      },
+      {
+        headers: { accept: "application/json" },
+      }
+    );
+
+    const accessToken = response.data.access_token;
+    if (!accessToken) {
+      return res.status(400).send("Failed to get access token");
+    }
+
+    const userResponse = await axios.get("https://api.github.com/user", {
+      headers: { Authorization: `token ${accessToken}` },
+    });
+    const emailResponse = await axios.get(
+      "https://api.github.com/user/emails",
+      {
+        headers: { Authorization: `token ${accessToken}` },
+      }
+    );
+
+    const githubUser = {
+      ...userResponse.data,
+      emails: emailResponse.data,
+    };
+
+    const provider = await prisma.provider.findFirst({
+      where: { name: "Github" },
+    });
+
+    let user = await prisma.user.findUnique({
+      where: { email: githubUser.emails[0].email },
+    });
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          email: githubUser.emails[0].email,
+          name: githubUser.name,
+          providerId: provider.id,
+        },
+        select: {
+          id: true,
+          bio: true,
+          email: true,
+          image: true,
+          createdAt: true,
+          name: true,
+        },
+      });
+    }
+    console.log(user);
+    res.redirect(process.env.GITHUB_CALLBACK_URL);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      status: false,
+      message: "Internal Server Error",
+      data: null,
+    });
+  }
+});
 
 module.exports = router;
